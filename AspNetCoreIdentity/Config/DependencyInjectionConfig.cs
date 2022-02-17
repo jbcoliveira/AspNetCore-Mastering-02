9919958c2f5af9bd8c5c99ce355ccde17de6ef6b
jbcoliveira <jbcoliveira1984@gmail.com>
2022-02-17 11:30:48 -0300

    Startup tunning

---
+++ AspNetCoreIdentity/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies (this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI(Microsoft.AspNetCore.Identity.UI.UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+            return services;
+        }
+    }
+}
--- AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/Startup.cs
@@ -1,19 +1,15 @@
 using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Config;
 using AspNetCoreIdentity.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreIdentity
 {
@@ -25,41 +21,18 @@ namespace AspNetCoreIdentity
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+       
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddIdentityConfig(Configuration);
 
-            services.AddDbContext<AspNetCoreIdentityContext>(options =>
-                    options.UseSqlServer(
-                        Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+            services.AddAuthorizationConfig();
 
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI(Microsoft.AspNetCore.Identity.UI.UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
-
-
-            services.AddAuthorization( options=>
-            {
-                options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
-                
-                options.AddPolicy("PodeLer", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeLer")));
-                options.AddPolicy("PodeEscrever", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeEscrever")));
-            }) ;
-
-            services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
+            services.ResolveDependencies();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -69,7 +42,6 @@ namespace AspNetCoreIdentity
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
